3618889697085cf56d301da85dad61e418176147
Carlos V <carlosvelazquezv2@gmail.com>
2025-05-27 11:46:07 -0700

    1.2

--- part1/newproject/src/App.jsx
+++ part1/newproject/src/App.jsx
@@ -12,7 +12,12 @@ const Content = (details) => {
   return (
     <div>
       <p>
-        {details.part} {details.exercises}
+        <p>{details.part1} {details.exercises1}</p>
+
+        <p>{details.part2} {details.exercises2}</p>
+
+        <p>{details.part3} {details.exercises3}</p>
+
       </p>
     </div>
   )
@@ -41,9 +46,7 @@ const App = () => {
   return (
     <div>
       <Header name={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
       <Total countPart1={exercises1} countPart2={exercises2} countPart3={exercises3}/>
     </div>
   )
