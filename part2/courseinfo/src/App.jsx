085e5e823249719a07565db268d73d4ef40d5b0f
Carlos V <carlosvelazquezv2@gmail.com>
2025-07-29 16:36:21 -0700

    Part 10

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,45 +1,4 @@
-const Header = (props) => <h1>{props.course}</h1>
-
-
-
-const Content = ({ props }) => {
-  console.log(props);
-  const values = props.map(part => part.exercises)
-  console.log(values);
-  
-  const total = values.reduce((s, p) => s + p, 0)
-  console.log(total);
-  
-   // Destructuring props directly here for cleaner access
-  return (
-    <div>
-      {props.map((part, i)=> ( // Use props.parts and wrap JSX in parentheses for implicit return
-        <p key={i}> {part.name} {part.exercises} </p> // Add a unique key when mapping over elements
-      ))}
-      <b>total of {total} exercises</b>
-    </div>
-  );
-};
-
-
-
-const Part = (props) => (
-  <p>
-    {props.part.name} {props.part.exercises}
-  </p>
-)
-
-const Total = (props) => <p>Number of exercises {props.total}</p>
-
-const Course = ({course}) => {
-console.log(course);
-
- return (
-  <div>
-      <Header course={course.name} />
-      <Content props={course.parts} />
-   </div> )
-  }
+import Course from "./components/Course"
 
 const App = () => {
   const courses = [
--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,22 +1,45 @@
+const Header = (props) => <h1>{props.course}</h1>
 
 
-const Header = ({ header }) => {
-  console.log(props)
+
+const Content = ({ props }) => {
+  console.log(props);
+  const values = props.map(part => part.exercises)
+  console.log(values);
+  
+  const total = values.reduce((s, p) => s + p, 0)
+  console.log(total);
+  
+   // Destructuring props directly here for cleaner access
   return (
-  <h1>  
-    {header.name}
-  </h1>
-  )
-}
+    <div>
+      {props.map((part, i)=> ( // Use props.parts and wrap JSX in parentheses for implicit return
+        <p key={i}> {part.name} {part.exercises} </p> // Add a unique key when mapping over elements
+      ))}
+      <b>total of {total} exercises</b>
+    </div>
+  );
+};
 
-const Course = ({ course }) => {
-  console.log(course)
-  return (
-  <div>  
-    <Header header ={course} />
-  </div>
-  )
-}
+
+
+const Part = (props) => (
+  <p>
+    {props.part.name} {props.part.exercises}
+  </p>
+)
+
+const Total = (props) => <p>Number of exercises {props.total}</p>
+
+const Course = ({course}) => {
+console.log(course);
+
+ return (
+  <div>
+      <Header course={course.name} />
+      <Content props={course.parts} />
+   </div> )
+  }
 
 
 export default Course
